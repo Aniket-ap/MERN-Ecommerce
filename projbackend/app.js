3807bd91c894e77be4149f85c16a4e6f2323e0f4
Aniket-ap <aniket_pro@outlook.com>
2020-06-17 16:57:46 +0530

    backend

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
