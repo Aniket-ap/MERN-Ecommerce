d68d78dae309e6aa61adeaa1b932d617fdf73b21
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-07 21:54:42 +0530

    a

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -4,11 +4,11 @@ const { ObjectId } = mongoose.Schema;
 const ProductCartSchema = new mongoose.Schema({
   product: {
     type: ObjectId,
-    ref: "Product"
+    ref: "Product",
   },
   name: String,
   count: Number,
-  price: Number
+  price: Number,
 });
 
 const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
@@ -22,8 +22,8 @@ const OrderSchema = new mongoose.Schema(
     updated: Date,
     user: {
       type: ObjectId,
-      ref: "User"
-    }
+      ref: "User",
+    },
   },
   { timestamps: true }
 );
