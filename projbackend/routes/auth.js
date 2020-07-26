9f9f67336d69749c5e68aaad3cb3de30386e800b
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-26 19:34:25 +0530

    a1

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { signout, signup, signin } = require("../controllers/auth");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 const { check, validationResult } = require("express-validator");
 
 router.post(
@@ -29,5 +29,9 @@ router.post(
 
 
 router.get("/signout", signout);
+
+router.get('/testroute', isSignedIn, (req, res)=>{
+  res.json(req.auth)
+})
 
 module.exports = router;
