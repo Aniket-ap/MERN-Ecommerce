86ae274c1a6acc521924eb9d3be026403137c42d
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-30 21:19:09 +0530

    frontend

---
+++ projfrontend/src/core/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div id="footer">
+        <span className="text-muted">
+          An Amazing <span className="text-white">T-Shirt</span> Store.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
